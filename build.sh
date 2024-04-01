ade5f80f3bb72d9b7ddc73a6c3099faa641453ae
Johan le stickman <johan@johanstick.fr>
2024-04-01 19:33:12 +0200

    Màj script de build

--- build.sh
+++ build.sh
@@ -1,35 +1,30 @@
 #!/bin/bash
 
-# Build
-echo "================= Building... 0/2 (Android)"
-flutter build apk --release
-echo "================= Building... 1/2 (iOS)"
-flutter build ios --release
-echo "================= Built."
-
-# Convert .app to .ipa (iOS)
-echo "================= Converting .app to .ipa"
-sh ./toipa.sh build/ios/iphoneos/Runner.app
-echo "================= Converted."
-
-# Add files to main build folder
-echo "================= Moving files to build folder"
-mv build/ios/iphoneos/Runner.ipa build/
-mv build/app/outputs/flutter-apk/app-release.apk build/
-echo "================= Moved."
-
-# Better naming
-echo "================= Renaming files"
 DATE=$(date +"%Y-%m-%d")
-rm -f build/StendMobile-ios-$DATE.ipa
-mv build/Runner.ipa build/StendMobile-ios-$DATE.ipa
+
+echo "================= 1/3 - Android"
+flutter build apk --release
+mv build/app/outputs/flutter-apk/app-release.apk build/
 rm -f build/StendMobile-android-$DATE.apk
 mv build/app-release.apk build/StendMobile-android-$DATE.apk
-echo "================= Renamed."
 
-# Logs files paths
+echo "================= 2/3 - iOS (Open)"
+flutter build ios --release
+sh ./toipa.sh build/ios/iphoneos/Runner.app
+mv build/ios/iphoneos/Runner.ipa build/
+rm -f build/StendMobile-ios-open-$DATE.ipa
+mv build/Runner.ipa build/StendMobile-ios-open-$DATE.ipa
+
+echo "================= 3/3 - iOS (Store)"
+flutter build ios --release --dart-define="storeRelease=true"
+sh ./toipa.sh build/ios/iphoneos/Runner.app
+mv build/ios/iphoneos/Runner.ipa build/
+rm -f build/StendMobile-ios-store-$DATE.ipa
+mv build/Runner.ipa build/StendMobile-ios-store-$DATE.ipa
+
 echo "================= Files paths:"
-echo "iOS:       $(pwd)/build/StendMobile-ios-$DATE.ipa"
-echo "Android:   $(pwd)/build/StendMobile-android-$DATE.apk"
-echo "Folder:    $(pwd)/build"
+echo "iOS (Store):  $(pwd)/build/StendMobile-ios-store-$DATE.ipa"
+echo "iOS (Open):   $(pwd)/build/StendMobile-ios-open-$DATE.ipa"
+echo "Android:      $(pwd)/build/StendMobile-android-open-$DATE.apk"
+echo "Folder:       $(pwd)/build"
 echo "================="
