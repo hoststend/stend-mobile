c15a36b2671bc9086b944412c2e6e106fa09dd34
Johan le stickman <johan@johanstick.fr>
2024-06-16 00:06:32 +0200

    Changement script build

--- build.sh
+++ build.sh
@@ -2,44 +2,37 @@
 
 DATE=$(date +"%Y-%m-%d")
 
-# fr.johanstick.stendmobile
+# Release Android - Store - APK
 echo "================= 1/5 - Android (Store - APK)"
-sed -i '' 's/fr.johanstick.stendmobile.dev/fr.johanstick.stendmobile/g' android/app/build.gradle
-sed -i '' 's/fr.johanstick.stendmobile.dev/fr.johanstick.stendmobile/g' android/app/src/main/kotlin/com/example/stendmobile/MainActivity.kt
 flutter build apk --release --dart-define="storeRelease=true"
 mv build/app/outputs/flutter-apk/app-release.apk build/
 rm -f build/StendMobile-android-$DATE-store.apk
 mv build/app-release.apk build/StendMobile-android-$DATE-store.apk
 
-# fr.johanstick.stendmobile
+# Release Android - Store - AAB
 echo "================= 2/5 - Android (Store - AAB)"
 flutter build appbundle --release --dart-define="storeRelease=true"
 mv build/app/outputs/bundle/release/app-release.aab build/
 rm -f build/StendMobile-android-$DATE-store.aab
 mv build/app-release.aab build/StendMobile-android-$DATE-store.aab
 
-# fr.johanstick.stendmobile.dev
+# Release Android - Open - APK
 echo "================= 3/5 - Android (Open - APK)"
-sed -i '' 's/fr.johanstick.stendmobile"/fr.johanstick.stendmobile.dev"/g' android/app/build.gradle
-sed -i '' '/fr\.johanstick\.stendmobile\.dev/! s/fr\.johanstick\.stendmobile/fr.johanstick.stendmobile.dev/g' android/app/src/main/kotlin/com/example/stendmobile/MainActivity.kt
 flutter build apk --release
 mv build/app/outputs/flutter-apk/app-release.apk build/
 rm -f build/StendMobile-android-$DATE-open.apk
 mv build/app-release.apk build/StendMobile-android-$DATE-open.apk
 
-# fr.johanstick.stendmobile
+# Release iOS - Store - IPA
 echo "================= 4/5 - iOS (Store - IPA)"
-# sed -i '' 's/fr.johanstick.stendmobile.dev/fr.johanstick.stendmobile/g' ios/Runner.xcodeproj/project.pbxproj
 flutter build ios --release --dart-define="storeRelease=true"
 sh ./utils/toipa.sh build/ios/iphoneos/Runner.app
 mv build/ios/iphoneos/Runner.ipa build/
 rm -f build/StendMobile-ios-$DATE-store.ipa
 mv build/Runner.ipa build/StendMobile-ios-$DATE-store.ipa
 
-# fr.johanstick.stendmobile.dev
+# Release iOS - Open - IPA
 echo "================= 5/5 - iOS (Open - IPA)"
-sed -i '' 's/fr.johanstick.stendmobile;/fr.johanstick.stendmobile.dev;/g' ios/Runner.xcodeproj/project.pbxproj
-sed -i '' 's/fr.johanstick.stendmobile.Runner/fr.johanstick.stendmobile.dev.Runner/g' ios/Runner.xcodeproj/project.pbxproj
 flutter build ios --release
 sh ./utils/toipa.sh build/ios/iphoneos/Runner.app
 mv build/ios/iphoneos/Runner.ipa build/
