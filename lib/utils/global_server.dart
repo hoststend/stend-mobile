fc0fd15e259f8f4f812f32ab8d0544e6f837ecfe
Johan le stickman <johan@johanstick.fr>
2024-08-15 03:30:21 +0200

    Possibilité cachée de changer l'API global server

--- lib/utils/global_server.dart
+++ lib/utils/global_server.dart
@@ -8,9 +8,10 @@ import 'package:url_launcher/url_launcher.dart';
 import 'package:http/http.dart' as http;
 
 final box = GetStorage();
+String baseUrl = box.read('globalserverInstanceUrl') ?? 'https://globalstend.johanstick.fr/';
 
 void openAuthGoogle({String responseType = 'redirect'}) {
-  launchUrl(Uri.parse('https://globalstend.johanstick.fr/auth/google/login?responseType=$responseType'), mode: LaunchMode.inAppBrowserView);
+  launchUrl(Uri.parse('${baseUrl}auth/google/login?responseType=$responseType'), mode: LaunchMode.inAppBrowserView);
 }
 
 void logout({ bool refreshSettings = true }) {
@@ -21,7 +22,7 @@ void logout({ bool refreshSettings = true }) {
 }
 
 Future checkcodeAuth(String code) async {
-  http.Response checkcode = await http.get(Uri.parse("https://globalstend.johanstick.fr/auth/checkcode?code=$code"));
+  http.Response checkcode = await http.get(Uri.parse("${baseUrl}auth/checkcode?code=$code"));
   final Map<String, dynamic> checkcodeJson = json.decode(utf8.decode(checkcode.bodyBytes));
 
   if (checkcodeJson['success'] != true) {
@@ -36,7 +37,7 @@ Future checkcodeAuth(String code) async {
   box.write('exposeMethods_account', true);
   box.write('exposeAccountToken', token);
 
-  http.Response checktransferts = await http.get(Uri.parse("https://globalstend.johanstick.fr/account/transferts"), headers: {'Authorization': token});
+  http.Response checktransferts = await http.get(Uri.parse("${baseUrl}account/transferts"), headers: {'Authorization': token});
   final Map<String, dynamic> checktransfertsJson = json.decode(utf8.decode(checktransferts.bodyBytes));
 
   if (checktransfertsJson['success'] != true) {
@@ -52,7 +53,7 @@ Future checkcodeAuth(String code) async {
 Future resetToken({ bool hapticFeedback = false }) async {
   if (hapticFeedback) Haptic().light();
 
-  http.Response resetToken = await http.post(Uri.parse("https://globalstend.johanstick.fr/account/reset"), headers: {'Authorization': box.read('exposeAccountToken')});
+  http.Response resetToken = await http.post(Uri.parse("${baseUrl}account/reset"), headers: {'Authorization': box.read('exposeAccountToken')});
   final Map<String, dynamic> resetTokenJson = json.decode(utf8.decode(resetToken.bodyBytes));
 
   if (resetTokenJson['success'] != true) {
@@ -70,7 +71,7 @@ Future resetToken({ bool hapticFeedback = false }) async {
   box.write('exposeMethods_account', true);
   box.write('exposeAccountToken', token);
 
-  http.Response checktransferts = await http.get(Uri.parse("https://globalstend.johanstick.fr/account/transferts"), headers: {'Authorization': token});
+  http.Response checktransferts = await http.get(Uri.parse("${baseUrl}account/transferts"), headers: {'Authorization': token});
   final Map<String, dynamic> checktransfertsJson = json.decode(utf8.decode(checktransferts.bodyBytes));
 
   if (checktransfertsJson['success'] != true) {
@@ -88,7 +89,7 @@ Future resetToken({ bool hapticFeedback = false }) async {
 Future deleteAccount({ bool hapticFeedback = false }) async {
   if (hapticFeedback) Haptic().light();
 
-  http.Response deleteAccount = await http.post(Uri.parse("https://globalstend.johanstick.fr/account/delete"), headers: {'Authorization': box.read('exposeAccountToken')});
+  http.Response deleteAccount = await http.post(Uri.parse("${baseUrl}account/delete"), headers: {'Authorization': box.read('exposeAccountToken')});
   final Map<String, dynamic> deleteAccountJson = json.decode(utf8.decode(deleteAccount.bodyBytes));
 
   debugPrint(deleteAccount.body.toString());
